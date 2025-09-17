8ea5826422240562fc6eaff8008fe92ceb04b146
Shaik Mastan <shaikmastan0093@gmail.com>
2025-09-17 14:52:41 +0530

    3d depth concepts

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-document.getElementById("signUp").addEventListener("click", function() {
-  alert("Redirect to Sign Up page...");
+const toSignUp = document.getElementById("toSignUp");
+const toLogin = document.getElementById("toLogin");
+const loginForm = document.querySelector(".login-container");
+const signupForm = document.querySelector(".signup-container");
+
+toSignUp.addEventListener("click", () => {
+  // Reset classes
+  loginForm.classList.remove("show-login", "hide-login");
+  signupForm.classList.remove("show-signup", "hide-signup");
+
+  // Apply proper classes
+  loginForm.classList.add("hide-login");
+  signupForm.classList.add("show-signup");
 });
+
+toLogin.addEventListener("click", () => {
+  // Reset classes
+  loginForm.classList.remove("show-login", "hide-login");
+  signupForm.classList.remove("show-signup", "hide-signup");
+
+  // Apply proper classes
+  signupForm.classList.add("hide-signup");
+  loginForm.classList.add("show-login");
+});
+
